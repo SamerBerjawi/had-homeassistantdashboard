53ded3e168eea67c8e1ddd128034e26ebe5a6c53
Samer Berjawi <berjawi@gmail.com>
2026-09-02 14:51:07 +0200

    refactor: remove all legacy dashboard components, utilities, and services

--- src/hooks/useEnergyDashboardData.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export * from './useEnergyData';
-export { useEnergyData as useEnergyDashboardData } from './useEnergyData';
-export type { EnergyHistoryPeriod as EnergyPeriod } from '../services/haEnergyStatistics';
--- src/services/haEnergyService.ts
+++
@@ -1,8 +0,0 @@
-/**
- * @license
- * SPDX-License-Identifier: Apache-2.0
- */
-
-export * from './haEnergyPreferences';
-export * from './haEnergyStatistics';
-export * from './energyDataTransformer';
